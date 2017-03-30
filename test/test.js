52cc044bcfa4ef0a931141965934c435faa87d79
Greg Coonrod <coogr02@ca.com>
2017-03-30 18:43:17 -0500

    Adding login handling and basic heartbeat sends to server

--- test/test.js
+++ test/test.js
@@ -4,24 +4,69 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Server = require('../lib/server');
+const Client = require('../lib/client');
 
-describe('Testing Server', function(){
-    before(function(done){
-        Server.start(() => {
+let server, client;
+
+describe('Tests', function() {
+    before(function(done) {
+        server = new Server(8192);
+        server.start(() => {
             console.log('Server started');
             done();
         });
     });
 
-    after(function(done){
-        Server.stop(error => {
+    after(function(done) {
+        server.stop(error => {
             console.log('Server stopped');
             done(error);
         });
     });
 
-    it('is alive', function(done){
-        console.log('Whee')
-        done();
+    it('is alive', function(done) {
+        client = new Client('127.0.0.1', 8192);
+        client.start(() => {
+            console.log('whee');
+            client.stop(() => {
+                done();
+            });
+        });
+    });
+
+    describe('Server Tests', function() {
+
+        beforeEach(function(done) {
+            client = new Client('127.0.0.1', 8192);
+            client.start(done)
+        });
+
+        afterEach(function(done) {
+            client.stop(done);
+        });
+
+        it('should accept a valid login', function(done) {
+            let login = {
+                name: 'Bob'
+            };
+
+            client._send(JSON.stringify(login), () => {
+                setTimeout(() => {
+                    expect(client.heartbeats.length).to.be.within(3, 5);
+                    done();
+                }, 5000);
+            });
+        });
+
+        it('should respond with an error when sent invalid json', function(done) {
+            let badJson = '{"foo": "bar"]';
+
+            client._send(badJson, () => {
+                setTimeout(() => {
+                    expect(client.errors.length).to.be.equal(1);
+                    done();
+                }, 5000);
+            });
+        });
     });
 });
