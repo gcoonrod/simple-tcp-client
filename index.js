dbbe6682dfccdd92852f56410fa4bb8fac24e3ff
Greg Coonrod <coogr02@ca.com>
2017-04-01 14:47:39 -0500

    Finished matching client to actual server responses

--- index.js
+++ index.js
@@ -1,33 +1,86 @@
-'use strict';
-const Client = require('./lib/client');
-const vorpal = require('vorpal')();
+'use strict'
+require('dotenv').config()
 
-const client = new Client('35.184.58.167', 9432, {logger: vorpal});
+const Client = require('./lib/client')
+const Util = require('./lib/util')
+const vorpal = require('vorpal')()
+const uuid = require('uuid')
 
-vorpal
-    .command('connect', 'connect')
-    .action((args, callback) => {
-        client.start(callback)
-    });
+const HOST = process.env.SERVER_HOST
+const PORT = process.env.SERVER_PORT
 
-vorpal
-    .command('login <name>', 'Login')
-    .action((args, callback) => {
-        let name = args.name;
-        client.login(name, callback);
-    });
+let client
+const id = uuid.v4()
 
-vorpal
-    .command('quit', 'quit')
-    .action(function(args) {
-        const self = this;
-        args.options = args.options || {};
-        args.options.sessionId = self.session.id;
-        client.stop(() => {
-            self.parent.exit(args.options)
-        });
-    });
+function getClient (mode) {
+  if (mode === 'development') {
+    return new Client('127.0.0.1', PORT, {logger: vorpal})
+  }
+
+  return new Client(HOST, PORT, {logger: vorpal})
+}
 
 vorpal
-    .delimiter('>>')
-    .show();
+  .command('connect')
+  .description('Connects to Help.com Test Server')
+  .action((args, callback) => {
+    client = getClient(process.env.NODE_ENV)
+    client.start(callback)
+  })
+
+vorpal
+  .command('login <name>')
+  .description('Logs in to Help.com Test Server with NAME')
+  .action((args, callback) => {
+    let name = args.name
+    client.login(name, () => {
+      vorpal.delimiter(`${name} >>`)
+      setInterval(Util.checkHeartbeat, 2000, client)
+      callback()
+    })
+  })
+
+vorpal
+  .command('get time')
+  .description('Requests current time from Help.com Test Server')
+  .action((args, callback) => {
+    client.requestTime(id, callback)
+  })
+
+vorpal
+  .command('get count')
+  .description('Requests count from Help.com Test Server')
+  .action((args, callback) => {
+    client.requestCount(id, callback)
+  })
+
+vorpal
+  .command('quit')
+  .description('Disconnects from Help.com Test Server and exits application')
+  .action(function (args) {
+    const self = this
+    args.options = args.options || {}
+    args.options.sessionId = self.session.id
+    client.stop(() => {
+      self.parent.exit(args.options)
+    })
+  })
+
+vorpal
+  .command('send <json>')
+  .description('Sends raw JSON to Help.com Test Server')
+  .action(function (args, callback) {
+    const self = this
+    let request
+    try {
+      request = JSON.parse(args.json)
+      client._send(request, callback)
+    } catch (error) {
+      self.log('Error parsing request string!', error)
+      callback(error)
+    }
+  })
+
+vorpal
+  .delimiter('>>')
+  .show()
--- test/test.js
+++ test/test.js
@@ -1,92 +1,91 @@
-'use strict';
+/* eslint-disable no-undef,no-unused-expressions */
+'use strict'
+require('mocha')
 
-const chai = require('chai');
-const expect = chai.expect;
+const chai = require('chai')
+const expect = chai.expect
 
-const Server = require('../lib/server');
-const Client = require('../lib/client');
+const Server = require('../lib/server')
+const Client = require('../lib/client')
 
-let server, client;
+let server, client
 
-describe('Tests', function() {
-    beforeEach(function(done) {
-        server = new Server(8192);
-        server.start(() => {
-            console.log('Server started');
-            done();
-        });
-    });
+describe('Tests', function () {
+  beforeEach(function (done) {
+    server = new Server(8192)
+    server.start(() => {
+      console.log('Server started')
+      done()
+    })
+  })
 
-    afterEach(function(done) {
-        server.stop(error => {
-            console.log('Server stopped');
-            done(error);
-        });
-    });
+  afterEach(function (done) {
+    server.stop(error => {
+      console.log('Server stopped')
+      done(error)
+    })
+  })
 
-    it('is alive', function(done) {
-        client = new Client('127.0.0.1', 8192);
-        client.start(() => {
-            console.log('whee');
-            client.stop(() => {
-                done();
-            });
-        });
-    });
+  it('is alive', function (done) {
+    client = new Client('127.0.0.1', 8192)
+    client.start(() => {
+      console.log('whee')
+      client.stop(() => {
+        done()
+      })
+    })
+  })
 
-    describe('Server Tests', function() {
+  describe('Server Tests', function () {
+    beforeEach(function (done) {
+      client = new Client('127.0.0.1', 8192)
+      client.start(done)
+    })
 
-        beforeEach(function(done) {
-            client = new Client('127.0.0.1', 8192);
-            client.start(done)
-        });
+    afterEach(function (done) {
+      client.stop(done)
+    })
 
-        afterEach(function(done) {
-            client.stop(done);
-        });
+    it('should accept a valid login', function (done) {
+      client.login('test1', () => {
+        setTimeout(() => {
+          expect(client.loggedIn).to.be.true
+          done()
+        }, 5000)
+      })
+    })
 
-        it('should accept a valid login', function(done) {
+    it('should respond with an error when sent invalid json', function (done) {
+      let badJson = '{"foo": "bar"]'
 
-            client.login('test1', () => {
-                setTimeout(() => {
-                    expect(client.loggedIn).to.be.true;
-                    done();
-                }, 5000);
-            });
+      client.login('test2', () => {
+        client._socket.write(badJson, () => {
+          setTimeout(() => {
+            expect(client.errors.length).to.be.equal(1)
+            done()
+          }, 5000)
+        })
+      })
+    })
 
-        });
+    it('should get a count of requests', function (done) {
+      client.login('test3', () => {
+        setTimeout(() => {
+          client.requestCount('test3', () => {
+            done()
+          })
+        }, 2000)
+      })
+    })
 
-        it('should respond with an error when sent invalid json', function(done) {
-            let badJson = '{"foo": "bar"]';
-
-            client.login('test2', () => {
-                client._socket.write(badJson, () => {
-                    setTimeout(() => {
-                        expect(client.errors.length).to.be.equal(1);
-                        done();
-                    }, 5000);
-                });
-            });
-        });
-
-        it('should get a count of requests', function(done) {
-            client.login('test3', () => {
-                setTimeout(() => {
-                    client.requestCount('test3', () => {
-                        done();
-                    });
-                }, 2000)
-            });
-        });
-
-        it('should get the time', function(done) {
-            client.login('test4', () => {
-                setTimeout(() => {
-                    client.requestTime('test4', () => {
-                        done();
-                    });
-                }, 2000);
-            });
-        });
-    });
-});
+    it('should get the time', function (done) {
+      client.login('test4', () => {
+        setTimeout(() => {
+          client.requestTime('test4', () => {
+            done()
+          })
+        }, 2000)
+      })
+    })
+  })
+})
